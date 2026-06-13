6b0b07fece029eb4c365b26279b4ba9249f83265
Augusto Brito <augusstobrito@gmail.com>
2026-06-13 00:17:50 -0300

    alterar porta do db no  docker compose

--- src/db/connection.factory.js
+++ src/db/connection.factory.js
@@ -3,6 +3,10 @@ export function criaConexaoDB(config) {
   console.debug(`Criando conexão com o banco de dados ${config.connection}...`);
   const connection = knex(config);
 
+  connection.raw('SELECT 1')
+  .then(() => console.log('Banco conectado'))
+  .catch(err => console.error('Erro banco:', err));
+
   connection[Symbol.dispose] = () => {
     console.log('Fechando conexão com o banco de dados...');
     return connection.destroy();
